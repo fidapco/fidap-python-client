ce8b49fa1161c1b8a46a932d168f9f3b93960c7a
arslanhashmi <m.arslanijaz9@gmail.com>
2021-01-27 15:26:23 +0530

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='fidap',
-    version='0.0.1',
+    version='0.0.2',
     description='Get financial data easily.',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
